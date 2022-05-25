ad571cbf77422360eb93ffe480dfd69f2927368a
Stein Milder <saj.milder@student.han.nl>
2022-05-25 14:10:23 +0200

    Firebase deleted

--- lib/view/chat_view.dart
+++ lib/view/chat_view.dart
@@ -4,7 +4,6 @@ import 'package:chatmodule/view/received_message_screen.dart';
 import 'package:chatmodule/view/send_message_screen.dart';
 import 'package:chatmodule/view_model/chat_viewmodel.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class ChatView extends StatefulWidget {
   final MessageDataSource messageDataSource;
@@ -25,7 +24,7 @@ class _ChatViewState extends State<ChatView> {
 
   @override
   void initState() {
-    _chatViewModel = context.read<ChatViewModel>();
+    _chatViewModel = ChatViewModel();
     _chatViewModel.setDataSource(widget.messageDataSource);
     _chatViewModel.startFetchingMessages('test');
 
@@ -40,61 +39,52 @@ class _ChatViewState extends State<ChatView> {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<ChatViewModel>(
-          create: (context) => ChatViewModel(),
-        ),
-      ],
-      child: Column(
-        children: [
-          Expanded(
-            child: Consumer<ChatViewModel>(
-              builder: (context, chatViewModel, child) => ListView(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                controller: _listViewController,
-                children: [
-                  for (Message message in chatViewModel.messages)
-                    message.isSender
-                        ? SentMessageScreen(message: message)
-                        : ReceivedMessageScreen(message: message)
-                ],
-              ),
-            ),
+    return Column(
+      children: [
+        Expanded(
+          child: ListView(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            controller: _listViewController,
+            children: [
+              for (Message message in _chatViewModel.messages)
+                message.isSender
+                    ? SentMessageScreen(message: message)
+                    : ReceivedMessageScreen(message: message)
+            ],
           ),
-          Container(
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                children: [
-                  TextField(
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      border: const OutlineInputBorder(),
-                      hintText: "Typ hier je bericht...",
-                      prefixIcon: IconButton(
-                        icon: const Icon(Icons.photo),
-                        onPressed: () => _chatViewModel.selectImage('test'),
-                      ),
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          _chatViewModel.sendMessage(
-                            prefix: 'test',
-                            text: _controller.text,
-                          );
-                          _controller.clear();
-                        },
-                        icon: const Icon(Icons.send),
-                      ),
+        ),
+        Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    hintText: "Typ hier je bericht...",
+                    prefixIcon: IconButton(
+                      icon: const Icon(Icons.photo),
+                      onPressed: () => _chatViewModel.selectImage('test'),
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        _chatViewModel.sendMessage(
+                          prefix: 'test',
+                          text: _controller.text,
+                        );
+                        _controller.clear();
+                      },
+                      icon: const Icon(Icons.send),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          )
-        ],
-      ),
+          ),
+        )
+      ],
     );
   }
 }
